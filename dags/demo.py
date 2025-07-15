1c36b3b3fd56f1717cc09208671c792617abd48e
zhou12138 <1721427560@qq.com>
2025-07-15 08:06:50 +0000

    install dependency

--- dags/demo.py
+++ dags/demo.py
@@ -26,11 +26,7 @@ with DAG(
 
     run_adf_pipeline = AzureDataFactoryRunPipelineOperator(
         task_id="run_my_adf_pipeline",
-        pipeline_name="your_pipeline_name",
-        resource_group_name="your_resource_group",
-        factory_name="your_data_factory_name",
-        parameters={"param1": "value1"},
-        azure_data_factory_conn_id="azure_default",  # Airflow 中配置的 Azure 连接
+        pipeline_name="AirflowtriggerTest",
         wait_for_termination=True,  # 是否等待 pipeline 执行完成
         deferrable=True  # 可选：使用 deferrable 模式释放 worker slot
     )
