0192cf607b9998479ecaaaf0b8a5a6630eda4351
zhou12138 <1721427560@qq.com>
2025-07-15 09:57:41 +0000

    version update

--- dags/demo.py
+++ dags/demo.py
@@ -27,6 +27,7 @@ with DAG(
     run_adf_pipeline = AzureDataFactoryRunPipelineOperator(
         task_id="run_my_adf_pipeline",
         pipeline_name="AirflowtriggerTest",
+        factory_name="ADFCMReplayV3",
         azure_data_factory_conn_id="azure_data_explorer_default",
         wait_for_termination=True,  # 是否等待 pipeline 执行完成
         deferrable=True  # 可选：使用 deferrable 模式释放 worker slot
