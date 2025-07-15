f6efe996b4ad74ed63500aee87ca7c293b0773b7
zhou12138 <1721427560@qq.com>
2025-07-15 03:38:49 +0000

    init airflow

--- dags/demo.py
+++ dags/demo.py
@@ -20,20 +20,4 @@ with DAG(
      hello_task2 = BashOperator(
         task_id='say_hello_yes1',
         bash_command='echo "Hello, Airflow!"')
-    hello_task=>hello_task2
-
-# 定义 DAG
-with DAG(
-    dag_id='demo_hello_world2',
-    start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
-    catchup=False,
-    tags=['example']
-) as dag:
-
-    # 定义一个任务
-     hello_task = BashOperator(
-        task_id='say_hello_yes1',
-        bash_command='echo "Hello, Airflow!"')
-
-
+    hello_task >> hello_task2
