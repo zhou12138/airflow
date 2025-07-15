8927c6f3a02c9e2b96c8c545cfed6eb9f50c3026
zhou12138 <1721427560@qq.com>
2025-07-15 03:35:53 +0000

    init airflow

---
+++ dags/demo.py
@@ -0,0 +1,33 @@
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# 定义 DAG
+with DAG(
+    dag_id='demo_hello_world',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=['example']
+) as dag:
+
+    # 定义一个任务
+     hello_task = BashOperator(
+        task_id='say_hello_yes',
+        bash_command='echo "Hello, Airflow!"')
+
+# 定义 DAG
+with DAG(
+    dag_id='demo_hello_world2',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=['example']
+) as dag:
+
+    # 定义一个任务
+     hello_task = BashOperator(
+        task_id='say_hello_yes1',
+        bash_command='echo "Hello, Airflow!"')
+
